b21b7723107cf95f13d63c900db0898379f4bf8d
alexpod1000 <alexpod1000@gmail.com>
2024-07-11 18:11:06 +0200

    feat: speeded up mps int8 matrix multiplication

--- quantization/utils_int8.py
+++ quantization/utils_int8.py
@@ -10,10 +10,19 @@ def quantize_fp32_linear_to_int8(linear_orig_weights):
     return int8_weight, scales.to(linear_orig_weights.dtype)
 
 
-def linear_int8(inputs, weight, scales, bias=None, original_shape=None):
+def linear_int8(inputs, weight_int8pack, scales, bias=None, original_shape=None):
+    # RuntimeError: _weight_int8pack_mm_mps : expect A to be 2D tensor.
+    if len(inputs.shape) == 3:
+        assert inputs.shape[0] == 1, "Only support BS=1 (torch problem with MPS)"
+    scales = scales.to(dtype=inputs.dtype)
+    out = torch._weight_int8pack_mm(inputs[0], weight_int8pack, scales)
+    if len(inputs.shape) == 3:
+        out = out.unsqueeze(0)
+    """
     out = torch.nn.functional.linear(inputs, weight.to(dtype=inputs.dtype))
     if scales is not None:
         out *= scales
+    """
     if bias is not None:
         out += bias
     return out
